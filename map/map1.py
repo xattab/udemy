36f5962542c289048c56a8d372ff82abb5de83d7
xattab <xattab@syneforge.com>
2017-07-31 18:36:38 +0300

    ALL

---
+++ map/map1.py
@@ -0,0 +1,25 @@
+import folium
+import pandas
+
+data=pandas.read_csv("Volcanoes.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+elev=list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map=folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
+fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=str(el)+" m", fill_color=color_producer(el)))
+
+map.add_child(fg)
+
+map.save("Map1.html")
