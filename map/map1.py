6343732b075cf4f4b950250cec67ebb7083dd78b
xattab <xattab@syneforge.com>
2017-08-01 15:42:50 +0300

    Final map

--- map/map1.py
+++ map/map1.py
@@ -1,5 +1,6 @@
 import folium
 import pandas
+import json
 
 data=pandas.read_csv("Volcanoes.txt")
 lat=list(data["LAT"])
@@ -15,11 +16,18 @@ def color_producer(elevation):
         return 'red'
 
 map=folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=str(el)+" m", fill_color=color_producer(el)))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=str(el)+" m", fill_color=color_producer(el)))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
 
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
